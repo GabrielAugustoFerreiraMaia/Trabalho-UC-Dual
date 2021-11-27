2602560179f29a28aed9088f93f3649962e32105
Gabriel Augusto <73672870+GabrielAugustoFerreiraMaia@users.noreply.github.com>
2021-11-27 16:15:06 -0300

    Acordion terminado

--- trabalhofinalgrupo15/lib/screens/info/info_faq.dart
+++ trabalhofinalgrupo15/lib/screens/info/info_faq.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
+
 import 'package:accordion/accordion.dart';
 
 class InfoFaq extends StatelessWidget {
@@ -12,38 +12,118 @@ class InfoFaq extends StatelessWidget {
         centerTitle: true,
       ),
       body: Accordion(
-        maxOpenSections: 3,
+        maxOpenSections: 1,
         headerPadding: EdgeInsets.symmetric(vertical: 7, horizontal: 15),
         children: [
           AccordionSection(
-            isOpen: true,
-            leftIcon: Icon(Icons.insights_rounded, color: Colors.white),
-            header: Text('Introduction'),
+            headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+            contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
+            isOpen: false,
+            leftIcon: Icon(Icons.help_outline, color: Colors.black),
+            header: Text(
+              'Como Solicitar um Guincho?',
+              style: TextStyle(color: Colors.black),
+            ),
             content: Text(
-              '_loremIpsum',
+              '1 - Entre em contato com a Rede Prime Automotive\n2 - Informe o ocorrido para o solicitação do guincho ',
+              style: TextStyle(color: Colors.black),
             ),
             contentHorizontalPadding: 20,
-            contentBorderWidth: 1,
+            contentBorderWidth: 0,
           ),
           AccordionSection(
+            headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+            contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
             isOpen: false,
-            leftIcon: Icon(Icons.compare_rounded, color: Colors.white),
+            leftIcon: Icon(Icons.help_outline, color: Colors.black),
             header: Text(
-              'About Us',
+              'Qual oficina eu devo encaminhar?',
+              style: TextStyle(color: Colors.black),
             ),
-            contentBorderColor: Color(0xffffffff),
-            content: Row(
-              children: [
-                Icon(Icons.compare_rounded,
-                    size: 120, color: Colors.orangeAccent),
-                Flexible(
-                    flex: 1,
-                    child: Text(
-                      '_loremIpsum',
-                    )),
-              ],
+            content: Text(
+              '1 - Verfique na tela de informações a oficina mais próxima a você',
+              style: TextStyle(color: Colors.black),
             ),
+            contentHorizontalPadding: 20,
+            contentBorderWidth: 0,
           ),
+          AccordionSection(
+            headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+            contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
+            isOpen: false,
+            leftIcon: Icon(Icons.help_outline, color: Colors.black),
+            header: Text(
+              'Quais ações devo fazer?',
+              style: TextStyle(color: Colors.black),
+            ),
+            content: Text(
+              '1 - Verfique a integridade e sáude dos envolvidos se necessário encaminha - los para um serviço de saúde\n2 - Registrar um boletim de ocorrência(link no aplicativo)\n 3 - Retirar seu veículo da via, se necessário solicitar um guincho',
+              style: TextStyle(color: Colors.black),
+            ),
+            contentHorizontalPadding: 20,
+            contentBorderWidth: 0,
+          ),
+          AccordionSection(
+            headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+            contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
+            isOpen: false,
+            leftIcon: Icon(Icons.help_outline, color: Colors.black),
+            header: Text(
+              'Como acompanhar os serviços a serem feito no carro?',
+              style: TextStyle(color: Colors.black),
+            ),
+            content: Text(
+              '1 - Verifique na tela de solicitações sobre o andamento de suas solicitações, pode conferir também através dos nossos canais de comunicação',
+              style: TextStyle(color: Colors.black),
+            ),
+            contentHorizontalPadding: 20,
+            contentBorderWidth: 0,
+          ),
+          AccordionSection(
+              headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+              contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
+              isOpen: false,
+              leftIcon: Icon(Icons.help_outline, color: Colors.black),
+              header: Text(
+                'Como solicitar um transporte extra?',
+                style: TextStyle(color: Colors.black),
+              ),
+              content: Text(
+                '1 - Entre em contato com a RedePrime,\npara conseguir um veiculo extra caso você nescessite de um em casos de urgência\n( Ex: Pt no carro ou socorro )',
+                style: TextStyle(color: Colors.black),
+              ),
+              contentHorizontalPadding: 20,
+              contentBorderWidth: 0),
+          AccordionSection(
+              headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+              contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
+              isOpen: false,
+              leftIcon: Icon(Icons.help_outline, color: Colors.black),
+              header: Text(
+                'O que fazer com terceiros?',
+                style: TextStyle(color: Colors.black),
+              ),
+              content: Text(
+                '1 - Fale com o causador do acidente para entrar em contato com a nossa rede de seguro, para poder negocioar o custo do prejuiso causado',
+                style: TextStyle(color: Colors.black),
+              ),
+              contentHorizontalPadding: 20,
+              contentBorderWidth: 0),
+          AccordionSection(
+              headerBackgroundColor: const Color.fromARGB(255, 210, 174, 109),
+              contentBackgroundColor: const Color.fromARGB(200, 210, 174, 109),
+              isOpen: false,
+              leftIcon: Icon(Icons.help_outline, color: Colors.black),
+              header: Text(
+                'Qual é minha franquia?',
+                style: TextStyle(color: Colors.black),
+              ),
+              content: Text(
+                '1 - Você pode verificar no seu contrato de adesão, perguntar nos nossos canais de atendimento',
+                style: TextStyle(color: Colors.black),
+              ),
+              contentHorizontalPadding: 20,
+              contentBorderWidth: 0),
         ],
       ),
     );
