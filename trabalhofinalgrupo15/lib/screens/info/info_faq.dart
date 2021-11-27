1ef992dbf85d1b66d85129fbb9a785cd02f25aa5
Gabriel Augusto <73672870+GabrielAugustoFerreiraMaia@users.noreply.github.com>
2021-11-27 15:28:41 -0300

    Acordion funcionando

--- trabalhofinalgrupo15/lib/screens/info/info_faq.dart
+++ trabalhofinalgrupo15/lib/screens/info/info_faq.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:accordion/accordion.dart';
 
 class InfoFaq extends StatelessWidget {
   @override
@@ -10,16 +11,39 @@ class InfoFaq extends StatelessWidget {
         title: const Text('FAQ'),
         centerTitle: true,
       ),
-      body: Column(
+      body: Accordion(
+        maxOpenSections: 3,
+        headerPadding: EdgeInsets.symmetric(vertical: 7, horizontal: 15),
         children: [
-          GFAccordion(
-              collapsedTitleBackgroundColor: Colors.grey,
-              expandedTitleBackgroundColor: Colors.red,
-              title: 'GF Accordion',
-              content:
-                  'GetWidget is an open source library that comes with pre-build 1000+ UI components.',
-              collapsedIcon: Icon(Icons.add),
-              expandedIcon: Icon(Icons.minimize))
+          AccordionSection(
+            isOpen: true,
+            leftIcon: Icon(Icons.insights_rounded, color: Colors.white),
+            header: Text('Introduction'),
+            content: Text(
+              '_loremIpsum',
+            ),
+            contentHorizontalPadding: 20,
+            contentBorderWidth: 1,
+          ),
+          AccordionSection(
+            isOpen: false,
+            leftIcon: Icon(Icons.compare_rounded, color: Colors.white),
+            header: Text(
+              'About Us',
+            ),
+            contentBorderColor: Color(0xffffffff),
+            content: Row(
+              children: [
+                Icon(Icons.compare_rounded,
+                    size: 120, color: Colors.orangeAccent),
+                Flexible(
+                    flex: 1,
+                    child: Text(
+                      '_loremIpsum',
+                    )),
+              ],
+            ),
+          ),
         ],
       ),
     );
